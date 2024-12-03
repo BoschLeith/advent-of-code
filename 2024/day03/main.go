cc7101548867590bf7f814ac84e5ca9bc9a829e6
BoschLeith <bosch.leith@gmail.com>
2024-12-03 16:22:27 -0600

    complete Day 3 Part 2

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -10,9 +10,11 @@ import (
 )
 
 func main() {
-	pattern := `mul\(\d{1,3},\d{1,3}\)`
-	re := regexp.MustCompile(pattern)
+	// Compile regular expressions for matching multiplication and control functions
+	regexPart1 := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	regexPart2 := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`)
 
+	// Open the input file
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -20,43 +22,76 @@ func main() {
 	}
 	defer file.Close()
 
-	// Read all lines at once
+	// Read lines from the file
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Process all lines
+	// Calculate and print results for both parts
+	fmt.Printf("Multiplication Result Total: %d\n", calculateResults(lines, regexPart1))
+	fmt.Printf("Enabled Multiplication Result Total: %d\n", calculateEnabledResults(lines, regexPart2))
+}
+
+// calculateResults computes the total multiplication result for the given regex
+func calculateResults(lines []string, regex *regexp.Regexp) int {
 	var total int
-	var result [][]int
-
 	for _, line := range lines {
-		matches := re.FindAllString(line, -1)
+		matches := regex.FindAllString(line, -1)
+		total += sumMultiplications(matches)
+	}
+	return total
+}
 
-		for _, expr := range matches {
+// calculateEnabledResults computes the total multiplication result considering enabled functions
+func calculateEnabledResults(lines []string, regex *regexp.Regexp) int {
+	var matches []string
+	for _, line := range lines {
+		matches = append(matches, regex.FindAllString(line, -1)...)
+	}
+
+	// Filter out the "do()" and "don't()" sections
+	filteredMatches := filterSlice(matches)
+	return sumMultiplications(filteredMatches)
+}
+
+// sumMultiplications sums the results of multiplication expressions
+func sumMultiplications(expressions []string) int {
+	var total int
+	for _, expr := range expressions {
+		if strings.HasPrefix(expr, "mul(") && strings.HasSuffix(expr, ")") {
 			numbers := strings.TrimSuffix(strings.TrimPrefix(expr, "mul("), ")")
 			numSlice := strings.Split(numbers, ",")
-
-			var sublist []int
-			for _, num := range numSlice {
-				if n, err := strconv.Atoi(num); err == nil {
-					sublist = append(sublist, n)
+			if len(numSlice) == 2 {
+				if n1, err1 := strconv.Atoi(numSlice[0]); err1 == nil {
+					if n2, err2 := strconv.Atoi(numSlice[1]); err2 == nil {
+						total += n1 * n2
+					}
 				}
 			}
-			result = append(result, sublist)
 		}
 	}
+	return total
+}
 
-	for i := range result {
-		total += result[i][0] * result[i][1]
+// filterSlice filters out expressions based on "do()" and "don't()" control functions
+func filterSlice(slice []string) []string {
+	var result []string
+	inDontSection := false
+	for _, item := range slice {
+		if item == "don't()" {
+			inDontSection = true
+		} else if item == "do()" {
+			inDontSection = false
+		}
+		if !inDontSection {
+			result = append(result, item)
+		}
 	}
-
-	fmt.Printf("Multiplication Result Total: %d\n", total)
-
+	return result
 }
